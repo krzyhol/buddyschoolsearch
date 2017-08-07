dfb261736adc0d0b4857a116240e64c29c42879d
Krzysztof Hołtyn <krzysztof.holtyn@droidsonroids.pl>
2017-08-07 13:33:26 +0200

    Initial Commit

---
+++ Buddy School Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buddy School Search
+//
+//  Created by AppStarter on 07.08.2017.
+//  Copyright © 2017 Krzysztof Hołtyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
